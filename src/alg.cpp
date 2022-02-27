c2c8aa4401cf1bf4cfece134828ed72939a665b5
PRIGARINA-VICTORIA <99180874+PRIGARINA-VICTORIA@users.noreply.github.com>
2022-02-27 14:52:44 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,16 +6,17 @@
 bool checkPrime(uint64_t value) {
 // вставьте код функции
   if (value > 1) {
-    for (uint64_t i = 2; i < value; i++) 
-    if (value % i == 0)
+  for (uint64_t i = 2; i < value; i++) {
+    if (value % i == 0) {
       return false;
-		
+   	
     return true;
-}
-  else
+  } else {
     return false;
+  }
 }
-
+}
+}
 uint64_t nPrime(uint64_t n) {
 // вставьте код функции
   uint64_t num = 0;
@@ -26,7 +27,6 @@ uint64_t nPrime(uint64_t n) {
     }
   }
 }
-
 uint64_t nextPrime(uint64_t value) {
 // вставьте код функции
   for (uint64_t i = value + 1; i++;) {
@@ -35,12 +35,11 @@ uint64_t nextPrime(uint64_t value) {
     }
   }
 }
-
 uint64_t sumPrime(uint64_t hbound) {
 // вставьте код функции
   uint64_t sum = 0;
   for (uint64_t i = 2; i < hbound; i++) {
-    if (checkPrime(i)){
+    if (checkPrime(i)) {
     sum += 1;
     }
   return sum;
